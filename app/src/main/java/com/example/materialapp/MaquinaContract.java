fa95c1de811179d68b5e109561fd26d34af58daa
Carlos <sa476536@uaeh.edu.mx>
2023-04-26 22:04:18 -0600

    Initial commit

---
+++ app/src/main/java/com/example/materialapp/MaquinaContract.java
@@ -0,0 +1,16 @@
+package com.example.materialapp;
+
+import android.provider.BaseColumns;
+
+public class MaquinaContract {
+    public static abstract class MaquinaEntry implements BaseColumns {
+        public static final String TABLE_NAME ="Maquina";
+
+        public static final String ID_MAQUINA = "id_maquina";
+        public static final String NOMBRE = "name";
+        public static final String DESCRIP = "descrip";
+        public static final String CLIENTE = "cliente";
+
+    }
+
+}
